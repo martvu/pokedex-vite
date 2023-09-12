341ff4b33e8dcf098c57256edb0bac3f503cfbcf
Martin Vu <martinvu@stud.ntnu.no>
2023-09-13 01:14:54 +0200

    functions for fetching data

---
+++ src/__test__/apiUtils.test.tsx
@@ -0,0 +1,23 @@
+import { describe, test, expect } from 'vitest'
+import { getPokemonData, getPokemonDataList } from '../utils/pokeApi'
+
+
+// TODO: Mock fetch calls
+describe('getPokemonData', () => {
+  test('should return pokemon data', async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/1/'
+    const result = await getPokemonData(url)
+    expect(result.id).toEqual(1)
+    expect(result.name).toEqual('bulbasaur')
+})})
+
+//TODO: Mock fetch calls
+describe('getPokemonDataList', () => {
+  test('should return list of pokemon data', async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+    const result = await getPokemonDataList(url)
+    expect(result.length).toEqual(151)
+    expect(result[0].id).toEqual(1)
+    expect(result[0].name).toEqual('bulbasaur')
+})})
+
