7dbc076da5366f27b3c94737558219d02f75d002
Martin Vu <martinvu@stud.ntnu.no>
2023-09-16 13:58:02 +0200

    test usage of usePokemonData hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
+import PokemonCard from './components/PokemonCard';
+import { usePokemonData } from './utils/pokeApi';
 
 function App() {
+  // access data from the usePokemonData hook
+  const { data, isError, isLoading, error } = usePokemonData('bulbasaur');
+  console.log(data);
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    const castedError = error as Error;
+    return <div>There was an error {castedError.message} </div>
+  }
+
+  if (!data) {
+    return <div>No data available yet...</div>;
+  }
   return (
     <div className='container'>
-      <div> Hello</div>
+      <PokemonCard pokemonInfo={data}/>
     </div>
   )
 }
