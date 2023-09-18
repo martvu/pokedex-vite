72b8a151c6667f8518458a5523cd90df2194bc09
Martin Vu <martinvu@stud.ntnu.no>
2023-09-18 16:52:38 +0200

    add id to path

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<PokemonList />} />
-          <Route path="/pokemon" element={<PokemonInfoPage />} />
+          <Route path="/pokemon/:id" element={<PokemonInfoPage />} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
