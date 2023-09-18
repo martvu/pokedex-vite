1c0d58702b78b99bd799941ba377b54d7e3cafcb
Martin Vu <martinvu@stud.ntnu.no>
2023-09-18 16:03:34 +0200

    add more tests, including snapshot

--- src/components/PokemonInfoPage.test.tsx
+++ src/components/PokemonInfoPage.test.tsx
@@ -1,24 +1,61 @@
-import { render, screen } from '@testing-library/react';
-import { describe, test } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PokemonInfoPage from './PokemonInfoPage';
+import userEvent from "@testing-library/user-event";
+import PokemonList from './PokemonList';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from '../App';
 
 const queryClient = new QueryClient();
 
-describe('PokemonInfoPage', () => {
-  test('renders the pokemon info page', async () => {
+describe('PokemonList', () => {
+  test('pokemonlist renders', async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <Router>
           <Routes>
-          <Route path="/" element={<PokemonInfoPage />} />
-        </Routes>
-      </Router>
+            <Route path="/" element={<PokemonList />} />
+          </Routes>
+        </Router>
       </QueryClientProvider>
       );
- 
-      await screen.findByText(/Bulbasaur/i);
+    
+      expect(await screen.findByText(/bulbasaur/i)).toBeInTheDocument();
+      expect(await screen.findByText(/#1/i)).toBeInTheDocument();
+      const elements = await screen.getAllByText('grass');
+      expect(elements).toHaveLength(2);
   });
 }) 
 
+describe('App', () => {
+  test('app routing works', async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+      );
+      await act(async () => {
+        userEvent.click(screen.getByText(/Pokemon Info Page/i));
+      });
+      expect(await screen.findByText(/#001/i)).toBeInTheDocument();
+      expect(await screen.findByText(/Description/i)).toBeInTheDocument(); 
+      
+      await act(async () => {
+        userEvent.click(screen.getByText(/Next/i));
+      }
+      );
+  });
+}) 
+
+//TODO snapshot testing (when ui is finalised)
+
+describe('Snapshot', () => {
+  test('snapshot', async () => {
+    const { asFragment } = render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+      );
+    expect(asFragment()).toMatchSnapshot();
+  })
+});
--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -2,75 +2,92 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.get('https://pokeapi.co/api/v2/pokemon/:id/', (req, res, ctx) => {
-
-  const id = req.params.id;
-  if (id === '1') {
+    const id = req.params.id;
+    if (id === '1') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 1,
+          name: 'bulbasaur',
+          types: [
+            {
+              slot: 1,
+              type: {
+                name: 'grass',
+                url: 'https://pokeapi.co/api/v2/type/12/',
+              },
+            },
+            {
+              slot: 2,
+              type: {
+                name: 'poison',
+                url: 'https://pokeapi.co/api/v2/type/4/',
+              },
+            },
+          ],
+          stats: [
+            {
+              base_stat: 45,
+              effort: 0,
+              stat: {
+                name: 'hp',
+                url: 'https://pokeapi.co/api/v2/stat/1/',
+              },
+            },
+            {
+              base_stat: 49,
+              effort: 0,
+              stat: {
+                name: 'attack',
+                url: 'https://pokeapi.co/api/v2/stat/2/',
+              },
+            }
+          ]
+        })
+      );
+    }
+    if (id === '2') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 2,
+          name: 'ivysaur',
+          types: [
+            {
+              slot: 1,
+              type: {
+                name: 'grass',
+                url: 'https://pokeapi.co/api/v2/type/12/',
+              },
+            },
+            {
+              slot: 2,
+              type: {
+                name: 'poison',
+                url: 'https://pokeapi.co/api/v2/type/4/',
+              },
+            },
+          ],
+        })
+      );
+    }
+    // Add more conditions for other Pokémon IDs as needed
+    if (id === '3') {
+      return res(
+        ctx.status(200), // Return a 404 response for unknown IDs
+        ctx.json({
+          id: 3,
+          name: 'Venusaur',
+        })
+      );
+    }
     return res(
-      ctx.status(200),
+      ctx.status(404), // Return a 404 response for unknown IDs
       ctx.json({
-        id: 1,
-        name: 'bulbasaur',
-        types: [
-          {
-            slot: 1,
-            type: {
-              name: 'grass',
-              url: 'https://pokeapi.co/api/v2/type/12/',
-            },
-          },
-          {
-            slot: 2,
-            type: {
-              name: 'poison',
-              url: 'https://pokeapi.co/api/v2/type/4/',
-            },
-          },
-        ],
+        name: 'Pokemon not found',
       })
     );
-  }
-  if (id === '2') {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        id: 2,
-        name: 'ivysaur',
-        types: [
-          {
-            slot: 1,
-            type: {
-              name: 'grass',
-              url: 'https://pokeapi.co/api/v2/type/12/',
-            },
-          },
-          {
-            slot: 2,
-            type: {
-              name: 'poison',
-              url: 'https://pokeapi.co/api/v2/type/4/',
-            },
-          },
-        ],
-      })
-    );
-  }
-
-  // Add more conditions for other Pokémon IDs as needed
-  if (id === '3') {
-  return res(
-    ctx.status(200), // Return a 404 response for unknown IDs
-    ctx.json({
-      id: 3,
-      name: 'Venusaur',
-    })
-  )};
-  return res(
-    ctx.status(404), // Return a 404 response for unknown IDs
-    ctx.json({
-      name: 'Pokemon not found',
-    })
-  )
-}),
+  }),
 
   rest.get('https://pokeapi.co/api/v2/pokemon', (req, res, ctx) => {
     const limit = req.url.searchParams.get('limit');
@@ -84,60 +101,163 @@ export const handlers = [
         })
       );
     }
-  }), 
+  }),
 
-  rest.get('https://pokeapi.co/api/v2/pokemon-species/:id/', (req, res, ctx) => {
-    const id = req.params.id;
-    if (id === '1') {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          flavor_text_entries: [
-            {
-              flavor_text:
-                'Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun’s\nrays, the seed grows progressively larger.',
-              language: {
-                name: 'en',
+  rest.get(
+    'https://pokeapi.co/api/v2/pokemon-species/:id/',
+    (req, res, ctx) => {
+      const id = req.params.id;
+      if (id === '1') {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            flavor_text_entries: [
+              {
+                flavor_text:
+                  'Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun’s\nrays, the seed grows progressively larger.',
+                language: {
+                  name: 'en',
+                },
               },
-            },
-          ],
-        })
-      );
-    }
-    if (id === '2') {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          flavor_text_entries: [
-            {
-              flavor_text:
-                'There is a bud on this Pokémon’s back. To support its weight, Ivysaur’s legs and trunk grow thick and strong.If it starts spending more time lying in the sunlight, it’s a sign that the bud will bloom into a large flower soon.',
-              language: {
-                name: 'en',
+            ],
+            genera: [
+              {
+                genus: 'Samen-Pokémon',
+                language: {
+                  name: 'de',
+                },
               },
-            },
-          ],
-        })
-      );
-    }
-    if (id === '3') {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          flavor_text_entries: [
-            {
-              flavor_text:
-                'There is a large flower on Venusaur’s back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower’s aroma soothes the emotions of people.',
-              language: {
-                name: 'en',
+              {
+                genus: 'Pokémon Semilla',
+                language: {
+                  name: 'es',
+                },
               },
-            },
-          ],
-        })
-      );
+              {
+                genus: 'Pokémon Graine',
+                language: {
+                  name: 'fr',
+                },
+              },
+              {
+                genus: '씨앗포켓몬',
+                language: {
+                  name: 'ko',
+                },
+              },
+              {
+                genus: 'SeedyPokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seeda Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seede Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seed Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+            ],
+          })
+        );
+      }
+      if (id === '2') {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            flavor_text_entries: [
+              {
+                flavor_text:
+                  'There is a bud on this Pokémon’s back. To support its weight, Ivysaur’s legs and trunk grow thick and strong.If it starts spending more time lying in the sunlight, it’s a sign that the bud will bloom into a large flower soon.',
+                language: {
+                  name: 'en',
+                },
+              },
+            ],
+            genera: [
+              {
+                genus: 'Samen-Pokémon',
+                language: {
+                  name: 'de',
+                },
+              },
+              {
+                genus: 'Pokémon Semilla',
+                language: {
+                  name: 'es',
+                },
+              },
+              {
+                genus: 'Pokémon Graine',
+                language: {
+                  name: 'fr',
+                },
+              },
+              {
+                genus: '씨앗포켓몬',
+                language: {
+                  name: 'ko',
+                },
+              },
+              {
+                genus: 'Seed Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seed Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seed Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+              {
+                genus: 'Seed Pokémon',
+                language: {
+                  name: 'en',
+                },
+              },
+            ],
+          })
+        );
+      }
+      if (id === '3') {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            flavor_text_entries: [
+              {
+                flavor_text:
+                  'There is a large flower on Venusaur’s back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower’s aroma soothes the emotions of people.',
+                language: {
+                  name: 'en',
+                },
+              },
+            ],
+          })
+        );
+      }
     }
-  }
-)];
+  ),
+];
 
 /* rest.get('https://pokeapi.co/api/v2/pokemon/2/', (req, res, ctx) => {
     return res(
@@ -147,5 +267,3 @@ export const handlers = [
       })
     );
   }), */
-
-  
