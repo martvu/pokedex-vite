22fbaf69528eccf0f6a098200f390c6c84cc6be3
karoline <karolisw@stud.ntnu.no>
2023-09-20 10:40:17 +0200

    Cleanup.

--- src/utils/__test__/pokemoncard.test.tsx
+++ src/utils/__test__/pokemoncard.test.tsx
@@ -26,57 +26,57 @@ Object.defineProperty(window, 'localStorage', { value: mockLocalStorage });
 
 const mockPikachu: Pokemon = {
   id: 25, 
-  name: "Pikachu", // Replace with the desired name
-  base_experience: 112, // Replace with the desired base experience
-  height: 4, // Replace with the desired height
-  is_default: true, // Replace with the desired value
-  order: 25, // Replace with the desired order
-  weight: 60, // Replace with the desired weight
+  name: "Pikachu", 
+  base_experience: 112, 
+  height: 4, 
+  is_default: true,
+  order: 25,
+  weight: 60,
   abilities: [
     {
-      is_hidden: false, // Replace with the desired value
-      slot: 1, // Replace with the desired slot
+      is_hidden: false,
+      slot: 1,
       ability: {
-        name: "Static", // Replace with the ability name
-        url: "https://pokeapi.co/api/v2/ability/1/", // Replace with the actual URL
+        name: "Static",
+        url: "https://pokeapi.co/api/v2/ability/1/", 
       },
     },
   ] as PokemonAbility[],
 
-  forms: [] as NamedAPIResource[], // Replace with the desired forms
+  forms: [] as NamedAPIResource[],
 
-  game_indices: [] as any[], // Replace with the desired game indices
+  game_indices: [] as any[], 
 
-  held_items: [] as any[], // Replace with the desired held items
+  held_items: [] as any[], 
 
-  location_area_encounters: "", // Replace with the desired location area encounters
+  location_area_encounters: "", 
 
-  moves: [] as any[], // Replace with the desired moves
+  moves: [] as any[], 
 
   sprites: {
-    front_default: "https://example.com/pikachu-front.png", // Replace with the desired sprite URLs
-    front_shiny: "https://example.com/pikachu-front-shiny.png", // Replace with the desired sprite URLs
-    front_female: "https://example.com/pikachu-front-female.png", // Replace with the desired sprite URLs
-    front_shiny_female: "https://example.com/pikachu-front-shiny-female.png", // Replace with the desired sprite URLs
-    back_default: "https://example.com/pikachu-back.png", // Replace with the desired sprite URLs
-    back_shiny: "https://example.com/pikachu-back-shiny.png", // Replace with the desired sprite URLs
-    back_female: "https://example.com/pikachu-back-female.png", // Replace with the desired sprite URLs
-    back_shiny_female: "https://example.com/pikachu-back-shiny-female.png", // Replace with the desired sprite URLs
+    front_default: "https://example.com/pikachu-front.png",
+    front_shiny: "https://example.com/pikachu-front-shiny.png", 
+    front_female: "https://example.com/pikachu-front-female.png", 
+    front_shiny_female: "https://example.com/pikachu-front-shiny-female.png", 
+    back_default: "https://example.com/pikachu-back.png",
+    back_shiny: "https://example.com/pikachu-back-shiny.png", 
+    back_female: "https://example.com/pikachu-back-female.png",
+    back_shiny_female: "https://example.com/pikachu-back-shiny-female.png", 
   } as PokemonSprites,
 
   species: {
-    name: "pikachu-species", // Replace with the species name
-    url: "https://pokeapi.co/api/v2/pokemon-species/25/", // Replace with the actual URL
+    name: "pikachu-species", 
+    url: "https://pokeapi.co/api/v2/pokemon-species/25/", 
   } as NamedAPIResource,
 
   stats: [
     {
       stat: {
-        name: "hp", // Replace with the stat name
-        url: "https://pokeapi.co/api/v2/stat/1/", // Replace with the actual URL
+        name: "hp", 
+        url: "https://pokeapi.co/api/v2/stat/1/", 
       },
-      effort: 0, // Replace with the effort value
-      base_stat: 35, // Replace with the base stat value
+      effort: 0,
+      base_stat: 35, 
     } as PokemonStat,
   ],
 
