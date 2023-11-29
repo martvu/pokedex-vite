908f6f65dff3fd4014451e18fe8baac649336386
Martin Vu <martinvu@stud.ntnu.no>
2023-11-29 22:20:43 +0100

    icons to assets

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,4 +1,23 @@
 /* eslint-disable react-refresh/only-export-components */
+import bug from '../assets/icons/bug.svg';
+import dark from '../assets/icons/dark.svg';
+import dragon from '../assets/icons/dragon.svg';
+import electric from '../assets/icons/electric.svg';
+import fairy from '../assets/icons/fairy.svg';
+import fighting from '../assets/icons/fighting.svg';
+import fire from '../assets/icons/fire.svg';
+import flying from '../assets/icons/flying.svg';
+import ghost from '../assets/icons/ghost.svg';
+import grass from '../assets/icons/grass.svg';
+import ground from '../assets/icons/ground.svg';
+import ice from '../assets/icons/ice.svg';
+import normal from '../assets/icons/normal.svg';
+import poison from '../assets/icons/poison.svg';
+import psychic from '../assets/icons/psychic.svg';
+import rock from '../assets/icons/rock.svg';
+import steel from '../assets/icons/steel.svg';
+import water from '../assets/icons/water.svg';
+
 export const MAX_NO_OF_POKEMON: number = 151;
 
 export const TYPES: string[] = [
@@ -24,24 +43,24 @@ export const TYPES: string[] = [
 ];
 
 export const TYPE_ICONS: Record<string, string> = {
-  normal: '/icons/normal.svg',
-  fire: '/icons/fire.svg',
-  water: '/icons/water.svg',
-  electric: '/icons/electric.svg',
-  grass: '/icons/grass.svg',
-  ice: '/icons/ice.svg',
-  fighting: '/icons/fighting.svg',
-  poison: '/icons/poison.svg',
-  ground: '/icons/ground.svg',
-  flying: '/icons/flying.svg',
-  psychic: '/icons/psychic.svg',
-  bug: '/icons/bug.svg',
-  rock: '/icons/rock.svg',
-  ghost: '/icons/ghost.svg',
-  dragon: '/icons/dragon.svg',
-  dark: '/icons/dark.svg',
-  steel: '/icons/steel.svg',
-  fairy: '/icons/fairy.svg',
+  normal: normal,
+  fire: fire,
+  water: water,
+  electric: electric,
+  grass: grass,
+  ice: ice,
+  fighting: fighting,
+  poison: poison,
+  ground: ground,
+  flying: flying,
+  psychic: psychic,
+  bug: bug,
+  rock: rock,
+  ghost: ghost,
+  dragon: dragon,
+  dark: dark,
+  steel: steel,
+  fairy: fairy,
 };
 
 export const TYPE_COLORS: Record<string, string> = {
